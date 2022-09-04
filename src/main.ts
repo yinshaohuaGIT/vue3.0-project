01a4e6af6617415c280f4728b92cd497627c539d
yinshaohuaGit <1490967674@qq.com>
2022-09-04 09:33:02 +0800

    依赖配置的首次提交

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import "./assets/main.css";
+import './assets/main.css'
+import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(router)
+app.use(store)
+app.use(ElementPlus)
 
-app.mount("#app");
+app.mount('#app')
